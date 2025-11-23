79ad5f3e04bd76e6ec6a6b32eb4a232023f6f2fc
sbx_user1051 <sbx_user1051@169.254.27.179>
2025-11-23 19:52:53 +0000

    Sun Nov 23 19:52:53 UTC 2025

--- test/solutions/RBT/test-rabbit-hole-solution.js
+++ test/solutions/RBT/test-rabbit-hole-solution.js
@@ -137,4 +137,109 @@ describe("RBT_R4: Room to Grow - Scaling", function () {
   });
 });
 
+describe("RBT_R5: Modernizing the Warren - Unicode Rendering", function () {
+  it("should render with ASCII theme by default", function () {
+    // Same as R4 test - default should still be ASCII
+    const digging = "DRDLDRRUURDD";
+    const expected =
+`+   +---+---+---+
+|       |       |
++---+   +   +   +
+|       |   |   |
++   +---+   +   +
+|           |   |
++---+---+---+---+
+`;
+    assert.equal(new RabbitHoleSolution().rabbit_hole(3, 4, digging, {}), expected);
+  });
+
+  it("should render with ASCII theme when explicitly specified", function () {
+    const digging = "DRDLDRRUURDD";
+    const options = { RENDERING_THEME: "ASCII" };
+    const expected =
+`+   +---+---+---+
+|       |       |
++---+   +   +   +
+|       |   |   |
++   +---+   +   +
+|           |   |
++---+---+---+---+
+`;
+    assert.equal(new RabbitHoleSolution().rabbit_hole(3, 4, digging, options), expected);
+  });
+
+  it("should render with Unicode theme - basic 3x4 maze", function () {
+    // From the challenge example with Unicode theme
+    const digging = "DRDLDRRUURDD";
+    const options = { RENDERING_THEME: "UNICODE" };
+    const expected =
+`╻   ╺━━━┳━━━━━━━┓
+┃       ┃       ┃
+┣━━━╸   ┃   ┃   ┃
+┃       ┃   ┃   ┃
+┃   ╺━━━┛   ┃   ┃
+┃           ┃   ┃
+┗━━━━━━━━━━━┻━━━┛
+`;
+    assert.equal(new RabbitHoleSolution().rabbit_hole(3, 4, digging, options), expected);
+  });
+
+  it("should render Unicode theme with custom scaling", function () {
+    const digging = "DRDLDRRUURDD";
+    const options = { RENDERING_THEME: "UNICODE", HORIZONTAL_SCALE: "6", VERTICAL_SCALE: "2" };
+    const expected =
+`╻      ╺━━━━━━┳━━━━━━━━━━━━┓
+┃             ┃             ┃
+┃             ┃             ┃
+┣━━━━━━╸      ┃      ┃      ┃
+┃             ┃      ┃      ┃
+┃             ┃      ┃      ┃
+┃      ╺━━━━━━┛      ┃      ┃
+┃                    ┃      ┃
+┃                    ┃      ┃
+┗━━━━━━━━━━━━━━━━━━━━┻━━━━━━┛
+`;
+    assert.equal(new RabbitHoleSolution().rabbit_hole(3, 4, digging, options), expected);
+  });
+
+  it("should render Unicode 1x1 cell", function () {
+    const digging = "D";
+    const options = { RENDERING_THEME: "UNICODE" };
+    const expected =
+`╻   ╻
+┃   ┃
+┗━━━┛
+`;
+    assert.equal(new RabbitHoleSolution().rabbit_hole(1, 1, digging, options), expected);
+  });
+
+  it("should render Unicode 2x2 with room", function () {
+    const digging = "DRDLU";
+    const options = { RENDERING_THEME: "UNICODE" };
+    const expected =
+`╻   ╺━━━┓
+┃       ┃
+┃       ┃
+┃       ┃
+┗━━━━━━━┛
+`;
+    assert.equal(new RabbitHoleSolution().rabbit_hole(2, 2, digging, options), expected);
+  });
+
+  it("should render Unicode with single-arm connectors", function () {
+    // Simple L-shape to test various connectors
+    const digging = "DR";
+    const options = { RENDERING_THEME: "UNICODE" };
+    const expected =
+`╻   ╺━━━┓
+┃       ┃
+┗━━━╸   ┃
+    ┃   ┃
+    ┗━━━┛
+`;
+    assert.equal(new RabbitHoleSolution().rabbit_hole(2, 2, digging, options), expected);
+  });
+});
+
+
 
