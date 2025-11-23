caa41cb642bee0215020173ea4f84cb73b740c1c
sbx_user1051 <sbx_user1051@169.254.14.87>
2025-11-23 18:13:13 +0000

    Sun Nov 23 18:13:13 UTC 2025

---
+++ test/solutions/RBT/test-rabbit-hole-solution.js
@@ -0,0 +1,9 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+import RabbitHoleSolution from "../../../lib/solutions/RBT/rabbit_hole_solution.js";
+
+describe("RBT challenge: draw empty ground", function () {
+  it("should draw nothing for 0x0", function () {
+    assert.equal(new RabbitHoleSolution().rabbit_hole(0, 0), "");
+  });
+});
